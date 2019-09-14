d0a985d01f2d838b59d8f697ea8e221f6b7866c3
mitsuhashi0921 <mitsuhashi0921@gmail.com>
2019-09-14 12:49:58 +0000

    enduserコントローラー、ビュー作成、ログイン後遷移画面は調整中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,16 @@ class ApplicationController < ActionController::Base
    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :email, :user_tell, :user_address, :postal_code])
  end
 
+   # ログインご画面についてはまた考える
+	def after_sign_in_path_for(resource)
+	    enduser_path(current_enduser.id)
+	    if resouce_or_scope.is_a?(Admin)
+	       home_path
+	    else
+	    	cds_index_path
+	    end
+	end
+
+end
+
 end
