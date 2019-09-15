cdb0fc04e3537659762882a1eb7c976c05ed1ca6
yamamotonaoki <pendonking4869@gmail.com>
2019-09-15 10:02:24 +0000

    seeds.rb作成

--- app/controllers/cds_controller.rb
+++ app/controllers/cds_controller.rb
@@ -1,13 +1,18 @@
 class CdsController < ApplicationController
   def index
+    @cds = Cd.all
   end
 
   def show
     @cds = Cd.find(params[:id])
   end
 
+  def cartin
+
+  end
+
   private
-  # def cds_params
-  #   params.require(:cd).permit(:label, :price, :genre)
-  # end
+  def cds_params
+    params.require(:cd).permit(:label, :genre)
+  end
 end
