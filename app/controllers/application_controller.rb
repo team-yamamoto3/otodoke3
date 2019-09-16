b1f84db9508ba2cdc27f892939a0b77cf3feeb09
mitsuhashi0921 <mitsuhashi0921@gmail.com>
2019-09-16 09:14:31 +0000

    ユーザー編集画面変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,14 @@ class ApplicationController < ActionController::Base
 	   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :email, :user_tell, :user_address, :postal_code])
 	 end
 
-   # ログインご画面についてはまた考える
-	def after_sign_in_path_for(resource)
-	  case resource
-      when Enduser
-        cds_index_path
-      when Admin
-        admin_root_path
-      end
-	end
+ #   # ログインご画面についてはまた考える
+	# def after_sign_in_path_for(resource)
+	#   case resource
+ #      when Enduser
+ #        home_root_path
+ #      when Admin
+ #        home_root_path
+ #      end
+	# end
 end
 
