1f9eeb70bc33a4938a8e193cd85daa02eb3c22c6
uetti-123 <mushibaitai123@gmail.com>
2019-09-16 08:05:10 +0000

    ログアウト時ルート、サインインエラー文の日本語化、エラー数合わせ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,23 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-	 def configure_permitted_parameters
-	   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :email, :user_tell, :user_address, :postal_code])
-	 end
-
-   # ログインご画面についてはまた考える
+  # ログインご画面についてはまた考える
 	def after_sign_in_path_for(resource)
 	  case resource
       when Enduser
         cds_index_path
       when Admin
-        admin_root_path
+        home_path
       end
 	end
+
+	def after_sign_out_path_for(resource)
+      cds_index_path
+    end
+
+
+  protected
+	 def configure_permitted_parameters
+	   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :email, :user_tell, :user_address, :postal_code])
+	 end
 end
