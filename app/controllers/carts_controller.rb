fac7c02f5ee89c94f02798764e648aa0ad6f853f
cfui <rckr34015@yahoo.co.jp>
2019-09-18 11:45:55 +0000

    カートに入れる機能追加

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,27 @@
 class CartsController < ApplicationController
   def index
+  	@carts = current_enduser.carts
   end
 
   def show
+  	@carts = current_enduser.carts.all
   end
 
   def create
+  	@cart = Cart.new(cart_params)
+  	@cart.enduser_id = current_enduser.id
+  	@cart.save
+  	redirect_to carts_show_path
   end
 
   def destroy
   end
+
+  private
+  def cart_params
+  	params.require(:cart).permit(:cartnumber, :cd_id, :enduser_id )
+  end
+
+
+
 end
