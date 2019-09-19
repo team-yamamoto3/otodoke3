f7316bac8024874345f240f56807d2007b2630a3
mitsuhashi0921 <mitsuhashi0921@gmail.com>
2019-09-19 04:37:43 +0000

    管理者側CD詳細、編集、削除機能実装

--- app/controllers/admin_cds_controller.rb
+++ app/controllers/admin_cds_controller.rb
@@ -1,24 +1,17 @@
 class AdminCdsController < ApplicationController
-    # before_action :authenticate_admin!
-
   def index
-    @cds = Cd.all.includes(:artists, :discs, :songs)
+    @cds = Cd.all.includes(:artists)
   end
 
   def new
     @cd = Cd.new
-    @disc = @cd.discs.build
-    @song = @disc.songs.build
     2.times {@cd.artists.build}
-    @sales_status = ["販売中", "販売停止中"]
-    @selectjenre = ["J-Pop", "K-Pop", "洋楽", "邦楽", "アニメ", "R&B", "ロック", "ハードロック", "パンク",
-       "EDM", "ヒップホップ", "レゲエ", "ジャズ", "ハードコア", "クラシック", "演歌"]
   end
 
   def create
     @cd = Cd.new(cd_params)
     @cd.save
-    redirect_to admin_cds_path
+    redirect_to 'cds/index'
     # リダイレクト先をpashで表示する
   end
 
@@ -28,7 +21,7 @@ class AdminCdsController < ApplicationController
 
   def index
     @q = Cd.ransack(params[:q])
-    @cds = @q.result(distinct: true)
+    @cds = @q.result(distinct: true).page(params[:page]).per(5).reverse_order
   end
 
   def edit
@@ -59,14 +52,14 @@ class AdminCdsController < ApplicationController
     redirect_to admin_cds_path
   end
 
-  def search
-  end
-
   private
   def cd_params
     params.require(:cd).permit(:sales_status, :price, :consumption_tax, :stock, :title, :jacket, :label, :genre,
         artists_attributes:[:artist],
         discs_attributes:[:id, :disc_name, :include, :disc_number, :_destroy,
         songs_attributes:[:id, :song, :songorder, :_destroy]])
+  end
+
+  def search
   end
 end
