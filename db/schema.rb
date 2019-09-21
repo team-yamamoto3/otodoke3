6c63a15f4c583140dc609a84fe6f8f45366fb309
yamamotonaoki <pendonking4869@gmail.com>
2019-09-21 09:09:11 +0000

    [Add]reciptsmodelにenduser_idカラム追加。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_081527) do
+ActiveRecord::Schema.define(version: 2019_09_21_090655) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "tell"
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 2019_09_19_081527) do
     t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "enduser_id"
   end
 
   create_table "songs", force: :cascade do |t|
