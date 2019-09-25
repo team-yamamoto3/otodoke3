0497ade3d02b728579301622073b8e9936d367c8
yamamotonaoki <pendonking4869@gmail.com>
2019-09-25 08:29:19 +0000

    [modified]cartshowのview

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -2,4 +2,8 @@ class Disc < ApplicationRecord
   belongs_to :cd
   has_many :songs, dependent: :destroy, inverse_of: :disc
   accepts_nested_attributes_for :songs, allow_destroy: true
+
+  validates :disc_name, presence: true
+  validates :include, presence: true
+  validates :disc_number, presence: true
 end
